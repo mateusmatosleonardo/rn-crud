2925f33a85e315a86f15ac4a45a0d9286a13d866
Mateus Leonardo <m4teusmatos@gmail.com>
2021-11-02 19:36:29 -0300

    style: styling the formlist

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,9 +1,69 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
-import {Text} from 'react-native';
+import {Text, TextInput, View, StyleSheet, Button} from 'react-native';
 
 const UserForm = ({route, navigation}) => {
   const [user, setUser] = useState(route.params ? route.params : {});
-  return <Text style={{color: '#222'}}>{user.id}</Text>;
+  return (
+    <View style={styles.form}>
+      <Text style={{color: '#222'}}>Nome</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={name =>
+          setUser({
+            ...user,
+            name,
+          })
+        }
+        placeholder="Informe o nome"
+        value={user.name}
+      />
+      <Text style={{color: '#222'}}>Email</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={email =>
+          setUser({
+            ...user,
+            email,
+          })
+        }
+        placeholder="Informe o email"
+        value={user.email}
+      />
+      <Text style={{color: '#222'}}>URL do avatar</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={avatarURL =>
+          setUser({
+            ...user,
+            avatarURL,
+          })
+        }
+        placeholder="Informe a URL do avatar"
+        value={user.avatarURL}
+      />
+      <Button
+        title="Salvar"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
+    </View>
+  );
 };
 
 export default UserForm;
+
+const styles = StyleSheet.create({
+  input: {
+    color: '#666',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 14,
+    borderRadius: 10,
+  },
+  form: {
+    padding: 12,
+  },
+});
