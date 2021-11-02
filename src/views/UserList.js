c1f7bdd803f075872e6ed6d94c2af6cb1913035f
Mateus Leonardo <m4teusmatos@gmail.com>
2021-11-02 18:58:41 -0300

    feat: creating function getActions
    creating function getActions for return the buttons

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import {FlatList, View} from 'react-native';
-import {Avatar, ListItem} from 'react-native-elements';
+import {Avatar, Button, ListItem, Icon} from 'react-native-elements';
 import users from '../data/users';
 
 const UserList = props => {
+  function getActions(user) {
+    return (
+      <>
+        <Button
+          onPress={() => {
+            props.navigation.navigate('UserForm', user);
+          }}
+          type="clear"
+          icon={<Icon name="edit" size={25} color="orange" />}
+        />
+        <Button
+          onPress={() => {
+            props.navigation.navigate('UserForm', user);
+          }}
+          type="clear"
+          icon={<Icon name="delete" size={25} color="red" />}
+        />
+      </>
+    );
+  }
+
   function getUserItem({item: user}) {
     return (
-      <ListItem bottomDivider>
+      <ListItem
+        bottomDivider
+        onPress={() => props.navigation.navigate('UserForm')}>
         <Avatar title={user.name} source={{uri: user.avatarURL}} rounded />
         <ListItem.Content>
           <ListItem.Title>{user.name}</ListItem.Title>
           <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
         </ListItem.Content>
+        {getActions(user)}
       </ListItem>
     );
   }
