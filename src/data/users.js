5e640241f3c661d11fc89b6764522aec6bbecae3
Mateus Leonardo <m4teusmatos@gmail.com>
2021-11-06 17:37:37 -0300

    feat: adding new users

--- src/data/users.js
+++ src/data/users.js
@@ -2,7 +2,7 @@ export default [
   {
     id: 1,
     name: 'Mateus',
-    email: 'mateusleo@gmail.com',
+    email: 'mateus@gmail.com',
     avatarURL:
       'https://i.pinimg.com/originals/8b/6e/c6/8b6ec60427f9b17c1d9aaf4c415babe3.png',
   },
@@ -31,6 +31,55 @@ export default [
     id: 5,
     name: 'Bia',
     email: 'bia@gmail.com',
+    avatarURL:
+      'https://i.pinimg.com/originals/8b/6e/c6/8b6ec60427f9b17c1d9aaf4c415babe3.png',
+  },
+  {
+    id: 6,
+    name: 'Júlia',
+    email: 'julia@gmail.com',
+    avatarURL:
+      'https://i.pinimg.com/originals/8b/6e/c6/8b6ec60427f9b17c1d9aaf4c415babe3.png',
+  },
+  {
+    id: 7,
+    name: 'Carlos',
+    email: 'carlos@gmail.com',
+    avatarURL:
+      'https://i.pinimg.com/originals/8b/6e/c6/8b6ec60427f9b17c1d9aaf4c415babe3.png',
+  },
+  {
+    id: 8,
+    name: 'Pedro',
+    email: 'pedro@gmail.com',
+    avatarURL:
+      'https://i.pinimg.com/originals/8b/6e/c6/8b6ec60427f9b17c1d9aaf4c415babe3.png',
+  },
+  {
+    id: 9,
+    name: 'João',
+    email: 'joao@gmail.com',
+    avatarURL:
+      'https://i.pinimg.com/originals/8b/6e/c6/8b6ec60427f9b17c1d9aaf4c415babe3.png',
+  },
+  {
+    id: 10,
+    name: 'Bianca',
+    email: 'bianca@gmail.com',
+    avatarURL:
+      'https://i.pinimg.com/originals/8b/6e/c6/8b6ec60427f9b17c1d9aaf4c415babe3.png',
+  },
+  {
+    id: 11,
+    name: 'Beatriz',
+    email: 'beatriz@gmail.com',
+    avatarURL:
+      'https://i.pinimg.com/originals/8b/6e/c6/8b6ec60427f9b17c1d9aaf4c415babe3.png',
+  },
+  {
+    id: 12,
+    name: 'Guilherme',
+    email: 'guilherme@gmail.com',
     avatarURL:
       'https://i.pinimg.com/originals/8b/6e/c6/8b6ec60427f9b17c1d9aaf4c415babe3.png',
   },
